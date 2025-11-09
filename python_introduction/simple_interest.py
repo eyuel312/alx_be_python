7d5381133f316d0ce97a3c975bb20f6e2f70d304
Eyuel A <gg7qhqvvt4@privaterelay.appleid.com>
2025-11-09 15:33:42 +0300

    Fix variable name typo in simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,5 @@
-prinicpal = 1000
+principal = 1000
 rate = 0.05
 time = 3
-simple interest = principal * rate * time
+interest = principal * rate * time
 print(f"The simple interest is: {interest}")
