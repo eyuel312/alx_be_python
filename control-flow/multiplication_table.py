4212a5b4853fbdd3363de91ce3cb009537e86019
Eyuel A <gg7qhqvvt4@privaterelay.appleid.com>
2025-11-17 10:43:20 +0300

    Rename variable 'result' to 'product'

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
 number = int(input("Enter a number to see its multiplicaton table: "))
 for i in range(1, 11):
-  result = number * i
-  print(f"{number} * {i} = {result}")
+  product = number * i
+  print(f"{number} * {i} = {product}")
