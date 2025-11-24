2b465353fc1ded8333ed2eed77b186f7aeccfa6b
Eyuel A <gg7qhqvvt4@privaterelay.appleid.com>
2025-11-24 15:32:49 +0300

    Implement shopping list manager functionality

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,47 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ").strip()
+
+        if choice == '1':
+            item = input("Enter the item to add: ").strip()
+            shopping_list.append(item)
+            print(f"'{item}' has been added to the list.\n")
+
+        elif choice == '2':
+            item = input("Enter the item to remove: ").strip()
+            if item in shopping_list:
+                shopping_list.remove(item)
+                print(f"'{item}' has been removed from the list.\n")
+            else:
+                print("Item not found in the list.\n")
+
+        elif choice == '3':
+            if not shopping_list:
+                print("Your shopping list is empty.\n")
+            else:
+                print("Current Shopping List:")
+                for i, item in enumerate(shopping_list, start=1):
+                    print(f"{i}. {item}")
+                print()
+
+        elif choice == '4':
+            print("Goodbye!")
+            break
+
+        else:
+            print("Invalid choice. Please try again.\n")
+
+
+if name == "__main__":
+  main()  
+    main()
