11e2e1a14ee1e4164e5baa1f1e0bbee14c01ecc7
Eyuel A <gg7qhqvvt4@privaterelay.appleid.com>
2025-11-24 16:08:35 +0300

    Fix docstring formatting in temperature conversion functions

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,19 +2,13 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
 def convert_to_celsius(fahrenheit):
-    """
-    Convert Fahrenheit to Celsius using the global factor.
-    """
+    """ Convert Fahrenheit to Celsius using the global factor. """
     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 
 def convert_to_fahrenheit(celsius):
-    """
-    Convert Celsius to Fahrenheit using the global factor.
-    """
+    """ Convert Celsius to Fahrenheit using the global factor. """
     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
-
-
 def main():
     temp_input = input("Enter the temperature to convert: ").strip()
     try:
