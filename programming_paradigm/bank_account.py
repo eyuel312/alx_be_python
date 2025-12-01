a11f49b2cf52628d7aca7f017dc8729609f0e371
Eyuel A <gg7qhqvvt4@privaterelay.appleid.com>
2025-12-01 20:50:20 +0300

    Modify display_balance to return balance string
    Change display_balance method to return balance as a string instead of printing.

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -10,4 +10,4 @@ class BankAccount:
           return True
           return False
           def display_balance(self):
-            print(f"Current balance: ${self.__account_balance}")
+            return f"Current Balance: ${self.__account_balance}"
