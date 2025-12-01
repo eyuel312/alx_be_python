b9dd190cafb7aae0f5ae034ce1802893aec08727
Eyuel A <gg7qhqvvt4@privaterelay.appleid.com>
2025-12-01 21:21:57 +0300

    Fix indentation and main check in main.py

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -1,12 +1,16 @@
 import sys
 from robust_division_calculator import safe_divide
+
 def main():
-  if len(sys.argv) !=3:
-    print("Usage: python main.py <numerator> <denominator>")
-    sys.exit(1)
+    if len(sys.argv) != 3:
+        print("Usage: python main.py <numerator> <denominator>")
+        sys.exit(1)
+
     numerator = sys.argv[1]
     denominator = sys.argv[2]
+
     result = safe_divide(numerator, denominator)
     print(result)
-    if __name == "__main__":
-      main()
+
+if name == "__main__":
+    main()
